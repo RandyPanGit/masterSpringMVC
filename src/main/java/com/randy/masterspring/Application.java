34daad4d4cd88ac4d7aa6f78c5f5e89062a22fbb
randy <randy4mac@gmail.com>
2017-07-04 21:58:55 +0800

    first thymeleaf example

--- src/main/java/com/randy/masterspring/Application.java
+++ src/main/java/com/randy/masterspring/Application.java
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class Application {
     @RequestMapping("/")
-    @ResponseBody
     String home() {
-        return "Hello World!";
+        return "resultPage";
     }
 
     public static void main(String[] args){
