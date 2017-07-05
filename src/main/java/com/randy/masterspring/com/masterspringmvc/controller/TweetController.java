17996f3efdfd985712e04f54b06fbb3f4c84ffdc
randy <randy4mac@gmail.com>
2017-07-05 21:44:35 +0800

    add TweetController

---
+++ src/main/java/com/randy/masterspring/com/masterspringmvc/controller/TweetController.java
@@ -0,0 +1,27 @@
+package com.randy.masterspring.com.masterspringmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by randy on 2017/7/5.
+ */
+@Controller
+public class TweetController {
+
+    @Autowired
+    private Twitter twitter;
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(defaultValue = "masterSpringMVC4")String search, Model model){
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        String text = searchResults.getTweets().get(0).getText();
+        model.addAttribute("message",text);
+        return "resultPage";
+    }
+}
