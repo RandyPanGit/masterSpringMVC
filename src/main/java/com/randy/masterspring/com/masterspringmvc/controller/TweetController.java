320640bd70d8f81f95349572279d839b200c8347
randy <randy4mac@gmail.com>
2017-07-06 21:51:12 +0800

    return result list

--- src/main/java/com/randy/masterspring/com/masterspringmvc/controller/TweetController.java
+++ src/main/java/com/randy/masterspring/com/masterspringmvc/controller/TweetController.java
@@ -2,11 +2,14 @@ package com.randy.masterspring.com.masterspringmvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by randy on 2017/7/5.
@@ -20,8 +23,9 @@ public class TweetController {
     @RequestMapping("/")
     public String hello(@RequestParam(defaultValue = "masterSpringMVC4")String search, Model model){
         SearchResults searchResults = twitter.searchOperations().search(search);
-        String text = searchResults.getTweets().get(0).getText();
-        model.addAttribute("message",text);
+        List<String> tweets = searchResults.getTweets().stream().map(Tweet:: getText).collect(Collectors.toList());
+
+        model.addAttribute("tweets",tweets);
         return "resultPage";
     }
 }
