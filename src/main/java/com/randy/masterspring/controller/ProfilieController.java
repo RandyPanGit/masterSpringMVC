2e0fbf2795816fabfd8b07790d078b76b32e0509
randy <randy4mac@gmail.com>
2017-07-16 22:31:19 +0800

    bind data

--- src/main/java/com/randy/masterspring/controller/ProfilieController.java
+++ src/main/java/com/randy/masterspring/controller/ProfilieController.java
@@ -1,7 +1,9 @@
 package com.randy.masterspring.controller;
 
+import com.randy.masterspring.model.ProfileForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by randy on 2017/7/16.
@@ -10,7 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProfilieController {
 
     @RequestMapping("profile")
-    public String displayProfile(){
+    public String displayProfile(ProfileForm profileForm){
         return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String saveProfile(ProfileForm profileForm){
+        System.out.print("sava ok");
+        return "redirect:/profile";
     }
 }
