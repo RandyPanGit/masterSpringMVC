f1ed967c51fdb7aa2cff0ff4eec2e21b69a829f1
randy <randy4mac@gmail.com>
2017-07-17 22:24:41 +0800

    欄位驗證

--- src/main/java/com/randy/masterspring/controller/ProfilieController.java
+++ src/main/java/com/randy/masterspring/controller/ProfilieController.java
@@ -3,10 +3,12 @@ package com.randy.masterspring.controller;
 import com.randy.masterspring.date.USLocalDateFormatter;
 import com.randy.masterspring.model.ProfileForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.Locale;
 
 /**
@@ -21,7 +23,10 @@ public class ProfilieController {
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(ProfileForm profileForm){
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "profile/profilePage";
+        }
         System.out.print("sava ok");
         return "redirect:/profile";
     }
