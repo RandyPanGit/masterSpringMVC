5bbf4f73b1d3b03a3953605370cd3bf397b48e82
randy <randy4mac@gmail.com>
2017-07-17 21:18:19 +0800

    add modelAttribute "dateFormat"

--- src/main/java/com/randy/masterspring/controller/ProfilieController.java
+++ src/main/java/com/randy/masterspring/controller/ProfilieController.java
@@ -1,9 +1,13 @@
 package com.randy.masterspring.controller;
 
+import com.randy.masterspring.date.USLocalDateFormatter;
 import com.randy.masterspring.model.ProfileForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
 
 /**
  * Created by randy on 2017/7/16.
@@ -20,5 +24,10 @@ public class ProfilieController {
     public String saveProfile(ProfileForm profileForm){
         System.out.print("sava ok");
         return "redirect:/profile";
+    }
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale){
+        return USLocalDateFormatter.getPattern(locale);
     }
 }
