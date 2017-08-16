0e4b46ef66e2573117e3d96533d9c62c20b7bcb7
randy <randy4mac@gmail.com>
2017-08-16 21:46:34 +0800

    swagger2

--- src/main/java/com/randy/masterspring/config/WebConfiguration.java
+++ src/main/java/com/randy/masterspring/config/WebConfiguration.java
@@ -17,6 +17,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.util.UrlPathHelper;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 
@@ -24,6 +27,7 @@ import java.time.LocalDate;
  * Created by randy on 2017/7/17.
  */
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -52,6 +56,14 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         objectMapper.configure(SerializationFeature.CLOSE_CLOSEABLE.WRITE_DATES_AS_TIMESTAMPS,false);
         return objectMapper;
+    }
+
+    @Bean
+    public Docket userApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(path -> path.startsWith("/api/"))
+                .build();
     }
 
     @Override
