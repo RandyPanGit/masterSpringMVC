09fffd25aa61a1202e216a5e4875fd34787af915
randy <randy4mac@gmail.com>
2017-08-16 21:36:11 +0800

    @ControllerAdvice

---
+++ src/main/java/com/randy/masterspring/error/EntityNotFountMapper.java
@@ -0,0 +1,16 @@
+package com.randy.masterspring.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EntityNotFountMapper {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entity clould not be found")
+    public void handleNotFound(){
+
+    }
+}
