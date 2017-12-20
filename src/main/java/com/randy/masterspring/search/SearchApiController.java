de92d55ae70cf519024f66d1c768df8c66185a2e
randy <randy4mac@gmail.com>
2017-12-20 16:06:14 +0800

    change interface

--- src/main/java/com/randy/masterspring/search/SearchApiController.java
+++ src/main/java/com/randy/masterspring/search/SearchApiController.java
@@ -10,15 +10,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/search")
 public class SearchApiController {
-    private SearchService searchService;
+    private TwitterSearch twitterSearch;
 
     @Autowired
-    public SearchApiController(SearchService searchService){
-        this.searchService = searchService;
+    public SearchApiController(TwitterSearch twitterSearch){
+        this.twitterSearch = twitterSearch;
     }
 
     @RequestMapping(value = "/{searchType}",method = RequestMethod.GET)
     public List<LightTweet> search(@PathVariable String searchType, @MatrixVariable List<String> keywords){
-        return searchService.search(searchType,keywords);
+        return twitterSearch.search(searchType,keywords);
     }
 }
