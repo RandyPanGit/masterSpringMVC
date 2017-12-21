81ea840fed732aa9af629030d22b4b196e14c583
randy <randy4mac@gmail.com>
2017-12-21 15:26:19 +0800

    add websocket configuration

---
+++ src/main/java/com/randy/masterspring/config/WebSocketConfiguration.java
@@ -0,0 +1,20 @@
+package com.randy.masterspring.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/ws");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/twitterSearch").withSockJS();
+
+    }
+}
